02fa6a23279e4c549ed7ed9c28374335c8afe1c4
modasser-nayem <modassernayem@gmail.com>
2025-05-26 22:24:36 +0600

    fix env variable prolem while testing

--- src/app/utils/validateEnv.ts
+++ src/app/utils/validateEnv.ts
@@ -1,7 +1,9 @@
 export function requireEnv(key: string): string {
   const value = process.env[key];
   if (!value) {
-    throw new Error(`Environment variable ${key} is missing or empty.`);
+    throw new Error(
+      `Environment variable ${key} is missing or empty. Did you forget to create a .env or set environment variables?`,
+    );
   }
   return value;
 }
@@ -10,7 +12,9 @@ export function requireNumberEnv(key: string): number {
   const value = requireEnv(key);
   const number = Number(value);
   if (isNaN(number)) {
-    throw new Error(`Environment variable ${key} must be a valid number.`);
+    throw new Error(
+      `Environment variable ${key} is missing or empty. Did you forget to create a .env or set environment variables?`,
+    );
   }
   return number;
 }
